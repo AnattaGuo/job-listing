9e4f6ab7b5a3b6aa522b1bfbfc1de083dddabc82
AnattaGuo <mengjunkwok@gmail.com>
2017-01-14 22:43:20 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
 
@@ -15,5 +17,4 @@ Rails.application.routes.draw do
   end
 
   root 'jobs#index'
-
 end
